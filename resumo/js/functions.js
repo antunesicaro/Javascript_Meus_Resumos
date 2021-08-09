c4048292f2fa8a702661af3409852fbf392f31ee
Ícaro Antunes <icarodasilva.12@icloud.com>
2021-08-09 15:10:43 -0300

    validação de formulário
    ainda está incompleto, logo logo estará finalizado.

--- resumo/js/functions.js
+++ resumo/js/functions.js
@@ -4628,8 +4628,137 @@ console.log(ControleRemoto.soma(10,20));
 
 controle1.aumentarVolume();
 console.log(controle1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//validando cpf com classe:
+class ValidaCPF{
+  constructor(cpfEnviado){
+    //definindo a propriedade do cpfLimpo, que ele nao vai poder ser modificado e que também já vai ser limpo com regex
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable:false,
+      enumerable:false,
+      configurable:false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    })
+  }
+
+  //criando os métodos
+
+  //método de checagem pra ver se o cpf não é 111.111.111-11 , ou qualquer sequencias do  tipo;
+  isSequencia(){
+    return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.lenght) === this.cpfLimpo //vai repetir o primeiro caracter q foi enviado do cpfLimpo(no caso o do indice 0)... vai repetir 11 vezes, que é o número de this.cpfLimpo.lenght. se isso for igual ao this.cpfLimpo, quer dizer que é uma sequencia, logo não pode ser um cpf válido
+  }
+
+    //gera novo cpf remover os dois digitos finais e fazer a contagem deles se estão de acordo com as normas do governo(calculo)
+    geraNovoCpf(){
+      //aqui se eu gerei, retorno true, se nao gerei, retorno false
+      //flag pra enviar pro método principal 'valida()'
+  
+      const cpfSemDigitosFinais = this.cpfLimpo.slice(0,-2);// corta o cpfLimpo começando do indice 0, dai corta -2 (os dois ultimos)
+      const digito1 = ValidaCPF.geraDigito(cpfSemDigitosFinais); //pega o cpf sem os dois digitos finais e manda pra um método de gerar o digito, para lá fazer a checagem se está de acordo com o calculo de cpf do governo
+      const digito2 = ValidaCPF.geraDigito(cpfSemDigitosFinais + digito1);
+      this.novoCPF = cpfSemDigitosFinais + digito1 + digito2;
+    }
+
+    //quando não uilido this no método, ele pode se tornar estático
+    static geraDigito(cpfSemDigitosFinais) {//recebe o cpf sem os digitos que veio como parametro do geraDigito(cpfSemDigitosFinais) do método geraNovoCpf
+            //fazendo um for pra iterar os digitos do cpf, ver que eles são em formato de string, converter pra number pra fazer as contas
+      let total = 0;
+      let reverso = cpfSemDigitosFinais.length + 1;//reverso pra fazer a conta com cpf do governo... começa no 10 quando tiver sem digito nenhum, começa do 11 quando tiver 1 digito
+  
+      for(let stringNumerica of cpfSemDigitosFinais) {//itera no for of, uma string numerica de todos numeros do cpfSemDigito
+        total += reverso * Number(stringNumerica); //transforma a stringNumerica em number vai fazendo a conta com cada reverso e com cada iterada da string numerica, ai isso tudo vai ficar em uma variavel total q começou em zero e vai guardar o valor
+        reverso--;//a cada iterada vai diminuindo o reverso para q seja o numero de fazer a conta do governo.. que é de 10 até 0 multiplicando com cada digito do cpf
+      }
+  
+      const digito = 11 - (total % 11);
+      return digito <= 9 ? String(digito) : '0';
+    }
+  
+
+
+  //método principal de validação do cpf
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpfLimpo !== 'string') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+    this.geraNovoCpf();
+    //console.log(this.novoCPF);
+
+    return this.novoCPF === this.cpfLimpo; //se for true , é válido
+  }
+
+
+}
+
+
+const cpf1 = new ValidaCPF('028.794.232-52');
+if(cpf1.valida() === true){
+  console.log('CPF válido')
+}else{
+  console.log('CPF inválido')
+}
+
 */
 
+//###IMPORTANTE
+//valindao formulário 
 
+class ValidaFormulario{
+  constructor(){
+    this.formulario = document.querySelector('.formulario'); //seleciona pela classe formulario
+    this.eventos()//já chama o método eventos que foi criado
+  }
 
+  //cria o método eventos
+  eventos(){
+    //adiciona um envento à classe formulario
+    this.formulario.addEventListener('submit',(e) => { //captura o evento de submit, ao clicar no submit vai executar uma função... passa uma função arrow pra que o this não seja perdido 
+      this.handleSubmit(e); //passa o evento de submit para o método handleSubmit, que irá ser um método que recebe como parametro o evento de submit, ai lá nele podemos fazer ações com o evento
+    });
+  }
 
+  handleSubmit(e){ //recebe o evento como parametro do método eventos
+    e.preventDefault(); // previne o padrão que  é enviar o formulário... não queremos enviar, pois ainda temos que checar se está tudo de acordo, valida-lo
+    //console.log('nao enviou o form quando cliquei no enviar'); //mostra que previniu enviar o form, nao foi enviado pois cancelamos o envio com prevent default
+  }
+}
+
+const valida = new ValidaFormulario(); //executa o valida formulario, ai quando executar ele já vai de cara chamar o this.eventos, ai vai começar os eventos de validação
